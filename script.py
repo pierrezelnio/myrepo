7893e8226a0529f2cf3cf63dfb91b6ea35962ef5
ZELNIO Pierre <zelnio.pierre@univ-catholyon.fr>
2020-02-18 15:08:07 +0100

    j'ai maintenant 2 questions

--- script.py
+++ script.py
@@ -3,7 +3,14 @@ print("T'avais pas des tomates à la maison")
 print("On veut pas de piques-assietes qui ont pas d'potes !")
 print("tuconnais")
 reponse = input("tu vas bien ?: ")
+reponse2 = input("T'aimes le chocolat ?: ")
+reponse3 = input("T'as déjà été en Argentine ?: ")
 if reponse == "y":
     print("Ok tant mieux !")
 elif reponse == "n":
     print("Dommage :(")
+
+if reponse2 == "y":
+    print("Moi aussi j'aime le chocolat")
+elif reponse2 == "n":
+    print("T'as raison c'est pas bon ...")
