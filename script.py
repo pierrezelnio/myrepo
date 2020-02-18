3303ca5f6e344ed563091c2697c4213677a59624
ZELNIO Pierre <zelnio.pierre@univ-catholyon.fr>
2020-02-18 15:04:22 +0100

    ajout d'une question

--- script.py
+++ script.py
@@ -2,4 +2,8 @@ print("Hello World !!")
 print("T'avais pas des tomates à la maison")
 print("On veut pas de piques-assietes qui ont pas d'potes !")
 print("tuconnais")
-int(input("tu vas bien ?: "))
+reponse = input("tu vas bien ?: ")
+if reponse == "y":
+    print("Ok tant mieux !")
+elif reponse == "n":
+    print("Dommage :(")
